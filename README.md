234b0f32f34d4442c24b5ae2f7461bfdd99f430b
Kyle Levy <37563214+kylelevy@users.noreply.github.com>
2022-10-23 20:06:23 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hi there!
+Thanks for taking an interest in what I've been up to. This site acts as an digital resume and an oppourtunity to learn a little bit more about me! I'll updating this site to hopefully feature some interesting projects.
+
+For now, you can open it to find my contact information and a link to my resume.
+
+Open the site [here](http://kylelevyofficial.github.io/ "here")!
---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+var btn = document.querySelector('#downloadBtn')
+
+function startDownload() {
+  btn.addEventListener('click', function() {
+    this.classList.add('is-active')
+    window.open('assets/Kyle_Levy_Resume.pdf', '_self');
+    setTimeout(function() {
+      btn.classList.add('dl-completed')
+      btn.classList.add('animation-ended')
+    }, 1200)
+  })
+}
+
+startDownload();
