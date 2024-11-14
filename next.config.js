1adb07c5385e0ea665c6d466c23134a612d85edd
kylelevy <kylerlevy@gmail.com>
2024-11-13 22:42:12 -0500

    gh no like :(

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 
 const nextConfig = {
 	distDir: 'dist',
-	experimental: {
-		staleTimes: {
-			dynamic: 30, // Re-fetch dynamic routes after 30 seconds
-			static: 180, // Re-fetch static routes after 3 minutes
-		},
-	},
 };
 
 module.exports = nextConfig;
