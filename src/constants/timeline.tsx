82d64d8895de9f2e8b0b1241d9316a9418191eea
kylelevy <kylerlevy@gmail.com>
2025-08-11 21:10:36 -0400

    Update mda experience

--- src/constants/timeline.tsx
+++ src/constants/timeline.tsx
@@ -2,12 +2,15 @@ export const timeline = [
   {
     company: "MDA Space",
     title: "Engineering Student",
-    date: "2024 - Present",
+    date: "2024 - 2025",
     description: "Building the space between proven and possible",
     responsibilities: [
-      "Robotics & Space Operations",
-      "Guidance, Navigation and Control",
-      "Research and Development - AI"
+      "AI/Machine Learning & Computer Vision: Spearheaded research, development, and presentation of cutting-edge AI methods for space industry applications, including anomaly detection, visual inspection, black-box optimization, and generative AI. Conducted extensive literature reviews on emerging AI technologies and translated research into practical implementations and potential corporate policy changes regarding the appropriate use of AI autonomy in a high-reliability sector with sensitive data concerns in space applications.",
+      "SoftwareEngineering & Backend Development: Designed, implemented, and documented robust software projects for on-premise cloud infrastructure supporting Large Language Models (LLMs). Architected and integrated secure authentication protocols (0Auth2 & OIDC) and high-redundancy databases to ensure data integrity and accessibility at scale. Delivered a fully featured LLM front-end and infrastructure, incorporating key features for user interaction and data management. Developed and maintained APIs utilizing SQL databases for authentication and access control.",
+      "DevOps & Infrastructure: Established and maintained comprehensive infrastructure tooling and DevOps processes on-premise. Founded the development of a multi-node compute cluster that significantly improved AI training times, with parallelization efforts accelerating genetic algorithm training by 16-64x. Automated CI/CD pipelines using Docker, Jenkins, and Kubernetes to facilitate rapid and reliable software deployments. Implemented robust security practices including certificate management, proxy configuration, and authentication protocols.",
+      "Robotics & Simulation: Prototyped and implemented realistic simulations of real-world robotic hardware and interactions, leveraging simulation results to improve physical system performance and inform decision-making. Utilized results to guide the design and optimization of robotic systems. Ran scripts and piloted space grade robots in the lab to run experiments and collect data for AI projects.",
+      "Technical Communication & Visualization: Delivered clear and comprehensive technical documentation for various projects using modern tools (Confluence). Created impactful data visualizations (3d rendering, plotting, PCA analysis) to analyze experimental results and identify correlations between parameters, supporting data-driven decision-making.",
+      "DataParsing: Engineered custom data pipelines and scripts to parse and process more than 100GB of data from diverse sources, creating a sustainable foundation for AI model training. Explored the implementation of safe agentic workflows for sensitive data with LLM tool calling and forked an open source project to extend its functionality",
     ],
   },
   {
